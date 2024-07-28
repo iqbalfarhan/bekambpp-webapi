695bce1b133ffe8335eca895cd024b85d8e137fb
iqbalfarhan <iqbalfarhan1996@gmail.com>
2024-07-28 12:57:55 +0800

    update icon dan title

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,8 +4,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="{{ url('logoimage.png') }}">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
+        <title>{{ $title ?? config('app.name') }}</title>
 
         @vite('resources/css/app.css')
     </head>
