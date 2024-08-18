4f0628d3f21f591b4a465967cc2fb86c0ed83f36
iqbalfarhan <iqbalfarhan1996@gmail.com>
2024-08-19 01:15:51 +0800

    udpate order api

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\OrderResource;
 use App\Models\Order;
+use App\Models\Sesi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
@@ -30,32 +31,41 @@ class OrderController extends Controller
     {
         $user_id = Auth::id();
         $request->validate([
-            'tanggal' => [
-                'required',
-                Rule::unique('orders')->where(function ($query) use ($user_id, $request) {
-                    return $query->where('user_id', $user_id)->where('sesi_id', $request->sesi_id);
-                })
-            ],
+            'tanggal' => 'required',
             'sesi_id' => 'required',
             'paket_id' => 'required',
             'keterangan' => '',
         ]);
 
+        $existingOrder = Order::where('tanggal', $request->tanggal)
+        ->where('sesi_id', $request->sesi_id)
+        ->where('user_id', $user_id)
+        ->first();
+
+        if ($existingOrder) {
+            $sesi = Sesi::find($request->sesi_id);
+            $tanggal = date('d F Y', strtotime($request->tanggal));
+            return response()->json([
+                'error' => "Mohon maaf booking sesi untuk jam {$sesi->jam_text} tanggal {$tanggal} tidak berhasil, mungkin anda melakukan booking diwaktu yang bersamaan sehingga waktu booking belum diperbarui. silakan pilih kembali waktu booking anda",
+            ], 400);
+        }
+
         try {
-            $order = Order::updateOrCreate([
+            $order = Order::create([
                 'tanggal' => $request->tanggal,
                 'user_id' => $user_id,
-            ],[
                 'sesi_id' => $request->sesi_id,
                 'paket_id' => $request->paket_id,
                 'keterangan' => $request->keterangan,
             ]);
-            return response()->json(Order::with('user', 'sesi', 'paket')->find($order->id));
+            $dataorder = Order::find($order->id);
+            return response()->json(new OrderResource($dataorder));
         } catch (\Exception $e) {
             return response()->json([
-                'error' => 'Terjadi kesalahan saat mengambil data sesi.',
+                'error' => 'Terjadi kesalahan saat menyimpan order.',
                 'message' => $e->getMessage()
             ], 500);
         }
     }
+
 }
